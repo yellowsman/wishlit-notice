749221c2fee04a7e05bf7de8d3ea84ca27354336
yellowsman <sigesigeito@gmail.com>
2016-03-20 19:12:13 +0900

    write parse code.

--- src/scrape/Scrape.java
+++ src/scrape/Scrape.java
@@ -1,10 +1,13 @@
 package scrape;
 
+import java.io.IOException;
+import java.io.StringBufferInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import config.Config;
@@ -16,8 +19,8 @@ import config.ScrapeParam;
 //  - cssQuery()を使って全件一致する方法を調べる(条件全てを使った検索)
 //  - 割引率のみならず割引額の場合を検討する
 
-
 public class Scrape {
+	final String url_domain = "http://www.amazon.co.jp/";
 
 	public static void main(String[] args) {
 		Scrape s = new Scrape();
@@ -38,56 +41,44 @@ public class Scrape {
 		int rate = 100;
 		String item_url = "";
 		String image_url = "";
-		
-
 		ArrayList<Item> item_list = new ArrayList<Item>();
 
 		for (Param p : sp.getParams()) {
-			String html = p.getWishlistUrl();
-			
-			// jsoupでp.getUrl()で取得したURLに対してparse処理
-			if(html.equals("") == false){
-				name = parseName(html);
-				price = parsePrice(html);
-				rate = parseRate(html);
-				item_url = parseItemUrl(html);
-				image_url = parseImageUrl(html);
-			}
+			try {
+				String html = Jsoup.connect(p.getWishlistUrl()).get().html();
+				Elements elements = parseItemList(html);
 
-			
-			// modeによって判定を変える
-			if (p.getMode().equals("price") && price <= p.getBorder()) {
-				item_list.add(new Item(
-						name, 
-						price, 
-						rate,
-						item_url,
-						image_url
-				));
-			} else if (p.getMode().equals("rate") && rate >= p.getBorder()) {
-				item_list.add(new Item(
-						name, 
-						price, 
-						rate,
-						item_url, 
-						image_url
-				));
+				// jsoupでp.getUrl()で取得したURLに対してparse処理
+				for (Element el : elements) {
+					name = parseName(el);
+					price = parsePrice(el);
+					rate = parseRate(el);
+					item_url = parseItemUrl(el);
+					image_url = parseImageUrl(el);
+				}
+
+				// modeによって判定を変える
+				if (p.getMode().equals("price") && price <= p.getBorder()) {
+					item_list.add(new Item(name, price, rate, item_url,
+							image_url));
+				} else if (p.getMode().equals("rate") && rate >= p.getBorder()) {
+					item_list.add(new Item(name, price, rate, item_url,
+							image_url));
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
-
 		return item_list;
 	}
 
-	
-	
 	// --------------------------------------
 	// メール送信処理
 	// --------------------------------------
 	boolean sendMail(List<Item> items, String address) {
 		return false;
 	}
-	
-	
+
 	// --------------------------------------
 	// パラメータ読み込み処理
 	// --------------------------------------
@@ -95,49 +86,66 @@ public class Scrape {
 		Config cd = new ConfigDummy();
 		return cd.readParams();
 	}
-	
+
 	// --------------------------------------
 	// パース処理
 	// --------------------------------------
-	Elements parseItems(){
+	Elements parseItemList(String html) {
 		// wish-listのアイテムを全取得
 		String selector_css = "a-fixed-left-grid   a-spacing-large";
-		return null;
-	}
-	
-	String parseName(String html){
 		Document doc = Jsoup.parse(html);
-		return doc.select(".a-link-normal a-declarative").text();
+		return doc.select(selector_css);
 	}
-	
-	int parsePrice(String html){
-		Document doc = Jsoup.parse(html);
-		return Integer.parseInt(doc.select(".a-size-base a-color-price a-text-bold").text()); // 金額まで取り出せていない
+
+	String parseName(Element element) {
+		return element.select(".a-link-normal a-declarative").text();
 	}
-	
+
+	int parsePrice(Element element) {
+		return Integer.parseInt(element.select(
+				".a-size-base a-color-price a-text-bold").text()); // 金額まで取り出せていない
+	}
+
 	// 割合か金額か判断
 	// 金額だった場合は元の値から割合を算出してから返す
-	int parseRate(String html){
-		Document doc = Jsoup.parse(html);
-		return Integer.parseInt(doc.select(".a-row itemPriceDrop").text());
+	int parseRate(Element element) {
+		String rate_word = element.select(".a-row itemPriceDrop").text();
+		if (rate_word.contains("%")) {
+			// sample => "値下がりしました: 9%"
+			int p1 = rate_word.indexOf(":");
+			int p2 = rate_word.indexOf("%");
+			return Integer.parseInt(rate_word.substring(p1, p2 - 1).trim());
+		} else {
+			// sample => "値下がりしました: ￥ 1 ほしい物リストに追加した時の価格は、￥724 でした"
+			int p1 = rate_word.indexOf("￥");
+			int p2 = rate_word.indexOf("ほ");
+			int low_price = Integer.parseInt(rate_word.substring(p1, p2 - 1)
+					.trim());
+
+			int p3 = rate_word.lastIndexOf("￥");
+			int p4 = rate_word.indexOf("で");
+			int origin_price = Integer.parseInt(rate_word.substring(p3, p4 - 1)
+					.trim());
+
+			return (low_price * 100) / origin_price;
+		}
 	}
-	
-	String parseItemUrl(String html){
-		Document doc = Jsoup.parse(html);
-		return null;
+
+	String parseItemUrl(Element element) {
+		return element.select(".a-link-normal a-declarative").attr("href");
 	}
-	
-	String parseImageUrl(String html){
-		Document doc = Jsoup.parse(html);
-		return null;
+
+	String parseImageUrl(Element element) {
+		return element.select(".a-link-normal a-declarative").first()
+				.attr("src");
 	}
-	
-	boolean isContainSellText(String str){
+
+	boolean isContainSellText(String str) {
 		return str.contains("値下がりしました:");
 	}
-	
+
 	// お気に入りリストに次にページがあればtrue
-	boolean isNextPage(Document doc){
-		return (doc.getElementById("wishlistPagination") != null);		
+	boolean isNextPage(Document doc) {
+		return (doc.getElementById("wishlistPagination") != null);
 	}
 }
