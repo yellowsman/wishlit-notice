920aa02427d05956c073e74502049aa7666ae6df
yellowsman <sigesigeito@gmail.com>
2016-03-21 06:53:34 +0900

    - add parse method class - add testClass of WishListParseTest - add testcase in WishListParseTest

--- src/scrape/Scrape.java
+++ src/scrape/Scrape.java
@@ -34,7 +34,7 @@ public class Scrape {
 		}
 	}
 
-	List<Item> scrape(ScrapeParam sp) {
+	public List<Item> scrape(ScrapeParam sparam) {
 		// dummy
 		String name = "";
 		int price = 100;
@@ -42,26 +42,27 @@ public class Scrape {
 		String item_url = "";
 		String image_url = "";
 		ArrayList<Item> item_list = new ArrayList<Item>();
+		WishListParse wp = new WishListParse();
 
-		for (Param p : sp.getParams()) {
+		for (Param param : sparam.getParams()) {
 			try {
-				String html = Jsoup.connect(p.getWishlistUrl()).get().html();
-				Elements elements = parseItemList(html);
+				String html = Jsoup.connect(param.getWishlistUrl()).get().html();
+				Elements elements = wp.parseItemList(html);
 
 				// jsoupでp.getUrl()で取得したURLに対してparse処理
 				for (Element el : elements) {
-					name = parseName(el);
-					price = parsePrice(el);
-					rate = parseRate(el);
-					item_url = parseItemUrl(el);
-					image_url = parseImageUrl(el);
+					name = wp.parseName(el);
+					price = wp.parsePrice(el);
+					rate = wp.parseRate(el);
+					item_url = wp.parseItemUrl(el);
+					image_url = wp.parseImageUrl(el);
 				}
 
 				// modeによって判定を変える
-				if (p.getMode().equals("price") && price <= p.getBorder()) {
+				if (param.getMode().equals("price") && price <= param.getBorder()) {
 					item_list.add(new Item(name, price, rate, item_url,
 							image_url));
-				} else if (p.getMode().equals("rate") && rate >= p.getBorder()) {
+				} else if (param.getMode().equals("rate") && rate >= param.getBorder()) {
 					item_list.add(new Item(name, price, rate, item_url,
 							image_url));
 				}
@@ -85,67 +86,5 @@ public class Scrape {
 	List<ScrapeParam> readConfig() {
 		Config cd = new ConfigDummy();
 		return cd.readParams();
-	}
-
-	// --------------------------------------
-	// パース処理
-	// --------------------------------------
-	Elements parseItemList(String html) {
-		// wish-listのアイテムを全取得
-		String selector_css = "a-fixed-left-grid   a-spacing-large";
-		Document doc = Jsoup.parse(html);
-		return doc.select(selector_css);
-	}
-
-	String parseName(Element element) {
-		return element.select(".a-link-normal a-declarative").text();
-	}
-
-	int parsePrice(Element element) {
-		return Integer.parseInt(element.select(
-				".a-size-base a-color-price a-text-bold").text()); // 金額まで取り出せていない
-	}
-
-	// 割合か金額か判断
-	// 金額だった場合は元の値から割合を算出してから返す
-	int parseRate(Element element) {
-		String rate_word = element.select(".a-row itemPriceDrop").text();
-		if (rate_word.contains("%")) {
-			// sample => "値下がりしました: 9%"
-			int p1 = rate_word.indexOf(":");
-			int p2 = rate_word.indexOf("%");
-			return Integer.parseInt(rate_word.substring(p1, p2 - 1).trim());
-		} else {
-			// sample => "値下がりしました: ￥ 1 ほしい物リストに追加した時の価格は、￥724 でした"
-			int p1 = rate_word.indexOf("￥");
-			int p2 = rate_word.indexOf("ほ");
-			int low_price = Integer.parseInt(rate_word.substring(p1, p2 - 1)
-					.trim());
-
-			int p3 = rate_word.lastIndexOf("￥");
-			int p4 = rate_word.indexOf("で");
-			int origin_price = Integer.parseInt(rate_word.substring(p3, p4 - 1)
-					.trim());
-
-			return (low_price * 100) / origin_price;
-		}
-	}
-
-	String parseItemUrl(Element element) {
-		return element.select(".a-link-normal a-declarative").attr("href");
-	}
-
-	String parseImageUrl(Element element) {
-		return element.select(".a-link-normal a-declarative").first()
-				.attr("src");
-	}
-
-	boolean isContainSellText(String str) {
-		return str.contains("値下がりしました:");
-	}
-
-	// お気に入りリストに次にページがあればtrue
-	boolean isNextPage(Document doc) {
-		return (doc.getElementById("wishlistPagination") != null);
 	}
 }
--- src/scrape_tset/ScrapeTest.java
+++ src/scrape_tset/ScrapeTest.java
@@ -1,4 +1,4 @@
-package scrape;
+package scrape_tset;
 
 import static org.junit.Assert.*;
 
@@ -8,6 +8,7 @@ import java.util.List;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import scrape.Scrape;
 import config.Param;
 import config.ScrapeParam;
 
@@ -24,7 +25,6 @@ public class ScrapeTest {
 		Param p1 = createParam("http://aaa.co.jp", "price", 0);
 		Param p2 = createParam("http://bbb.co.jp", "price", 1000);
 		Param p3 = createParam("http://ccc.co.jp", "rate", 10);
-		Param p4 = createParam("http://ddd.co.jp", "rate", 100);
 		Param p5 = createParam("http://eee.co.jp", "rate", 0);
 		
 		
@@ -32,7 +32,6 @@ public class ScrapeTest {
 		params.add(p1);
 		params.add(p2);
 		params.add(p3);
-		params.add(p4);
 		params.add(p5);
 		
 		
@@ -41,24 +40,13 @@ public class ScrapeTest {
 		scrape.scrape(sp);
 	}
 	
-	@Test
-	public void testParse(){
-		Scrape scrape = new Scrape();
-		String html = "";
-		
-		
-		
-		
-	}
 	
 	
-	
-	@Test
 	public ScrapeParam createScrapeParam(String userId,String address,List<Param> params){
 		return new ScrapeParam(userId,address,params);
 	}
 	
-	@Test
+
 	public Param createParam(String url,String mode,int border){
 		return new Param(url,mode,border);
 		
